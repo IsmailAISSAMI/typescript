66b7f7e2aa8a576ae4462673cd7299773d6518de
ismailaissami <aissami.ismail@gmail.com>
2022-01-27 14:45:53 +0100

    TS configue

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("Hello m**F**");
+const myVar = 20;
+console.log(`test var${myVar}`);
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log("Hello m**F**");
+const myVar = 20
+console.log(`test var${myVar}`);
