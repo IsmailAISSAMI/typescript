b8a48560778e3b3c89278122a14b0bb5b4151ee6
ismailaissami <aissami.ismail@gmail.com>
2022-01-27 15:20:51 +0100

    update

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,42 @@
 "use strict";
 console.log("Hello m**F**");
-const myVar = 20;
-console.log(`test var${myVar}`);
+let x = 20;
+console.log(`test var${x}`);
+// x="is a string"; ERROR
+let ref;
+// ref=23; ERROR
+ref = "no problem";
+console.log(ref);
+let ref2;
+ref2 = 23;
+ref2 = false;
+console.log(ref2);
+function getRef(arg) {
+    console.log(arg);
+}
+// equivalent to the previous function 
+// function getRef(arg: string):void{
+//     console.log(arg);
+// }
+function getRefWithReturn(arg) {
+    console.log(arg);
+    return arg;
+}
+// getRef(ref2); ERROR: ref2 is a boolean 
+getRef(ref);
+getRefWithReturn('return string');
+function get3(arg) {
+    console.log(arg);
+    return arg;
+}
+get3(122);
+let c1 = {
+    name: "aissami",
+    age: 44
+};
+let c2 = {
+    name: "thomas",
+    age: 44
+};
+console.log(c1);
+console.log(c2);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
 console.log("Hello m**F**");
-const myVar = 20
-console.log(`test var${myVar}`);
+let x = 20;
+console.log(`test var${x}`);
+
+// x="is a string"; ERROR
+
+let ref: string;
+// ref=23; ERROR
+ref="no problem"
+console.log(ref);
+
+let ref2: string | number | boolean;
+ref2=23
+ref2=false
+console.log(ref2);
+
+function getRef(arg: string){
+    console.log(arg);
+}
+
+// equivalent to the previous function 
+// function getRef(arg: string):void{
+//     console.log(arg);
+// }
+
+function getRefWithReturn(arg: string):string{
+    console.log(arg);
+    return arg;
+}
+// getRef(ref2); ERROR: ref2 is a boolean 
+getRef(ref);
+getRefWithReturn('return string');
+
+function get3(arg: string|number):string|number{
+    console.log(arg);
+    return arg;
+}
+get3(122);
+
+
+type Customer = {
+    name: string,
+    age?: number // with ? age now is optional
+};
+let c1:Customer={
+    name:"aissami",
+    age:44
+};
+let c2:Customer={
+    name:"thomas",
+    age:44
+};
+console.log(c1);
+console.log(c2);
+
